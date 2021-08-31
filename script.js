2aa948384a6e5fe05d3e59960a12cfd609fbd6b2
Stefanos Gkouveris <sgouver@hotmail.com>
2021-08-31 17:30:21 +0100

    Added data full name and position

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const getData = async () => {
     const res = await fetch('./data.json');
     const data = await res.json();
 
+    console.log(data)
     return data;
 };
 
@@ -10,6 +11,7 @@ const playerList = async () => {
     const temp = document.querySelector("template");
     const selectNode = temp.content.querySelector('#player-select');
     const imageSector = document.querySelector('#player-img');
+    const infoSector = document.querySelector('#player-info');
 
     dataInJson.players.forEach(card => {
         const playerID = `${card.player.id}`;
@@ -21,14 +23,28 @@ const playerList = async () => {
 
     document.querySelector("header").appendChild(selectNode);
 
-    const addImage = (id) => {
-        const image = `<img src="./assets/p${id}.png" alt="image of" width="500" height="600">`
-        imageSector.innerHTML = image;
+    const addData = (id) => {
+        const playerName = infoSector.querySelector('#player-info-name');
+        const playerInfo = infoSector.querySelector('#player-info-position');
+
+        dataInJson.players.forEach(card => {
+            if (card.player.id == id) {
+                const fullName = `${card.player.name.first} ${card.player.name.last}`;
+                const image = `<img src="./assets/p${card.player.id}.png" alt="image of ${fullName}" width="500" height="600">`
+                const position = `${card.player.info.positionInfo}`
+                
+
+                imageSector.innerHTML = image;
+                playerName.innerHTML = fullName;
+                playerInfo.innerHTML = position;
+            }
+        })
+
     }
 
     const selectPlayer = () => {
         selectNode.addEventListener("change", function(e) {
-            addImage(selectNode.value);
+            addData(selectNode.value);
         });
     }
     
